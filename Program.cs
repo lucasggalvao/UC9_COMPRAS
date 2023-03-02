27154ce7a38cc7aaa384993c47eef96c31f7800c
LUCAS GOMES GALVAO <lucas.ggalvao@senacsp.edu.br>
2023-03-01 21:53:47 -0300

    compras

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Criar variáveis necessárias
+double produto, total, totaldesconto;
+int i;
+
+// Inicializar Variáveis
+total = 0;
+totaldesconto = 0;
+
+// Informar valores de produtos
+for (i = 1; i <= 5; i++)
+{
+    Console.WriteLine("Informe o " + i + "º valor de produto");
+    produto = double.Parse(Console.ReadLine());
+    total = total + produto;
+    Console.Clear();
+    Console.WriteLine("O Total atual é: " + total);
+}
+
+totaldesconto = total - (total * 0.347);
+Console.WriteLine("O Total com desconto de 34,7% é: " + totaldesconto);
